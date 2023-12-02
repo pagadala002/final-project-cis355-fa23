ac6b29f892f2da0ab5b69c5f806b4787245686dd
pagadala002 <91543644+pagadala002@users.noreply.github.com>
2023-12-02 08:50:17 +0500

    Added XML Comments and Status Codes with Login Endpoint in Auth Controller
    Added XML Comments and Status Codes with Login Endpoint in Auth Controller

--- backend/src/UserApi/Controllers/AuthController.cs
+++ backend/src/UserApi/Controllers/AuthController.cs
@@ -16,6 +16,18 @@ public class AuthController : ControllerBase
         _userService = userService;
     }
 
+    /// <summary>
+    /// Authenticates a user based on provided credentials.
+    /// </summary>
+    /// <remarks>
+    /// This endpoint processes the authentication request. If credentials are valid, it returns an authentication token; 
+    /// otherwise, it responds with an error message.
+    /// </remarks>
+    /// <param name="model">The authentication request containing the user's credentials.</param>
+    /// <response code="200">Authentication successful. Returns the auth token.</response>
+    /// <response code="400">Bad request. The request body may be missing or with incorrect data.</response>
+    /// <response code="401">Unauthorized. The username or password is incorrect.</response>
+    /// <response code="500">Internal Server Error. Could occur if there is an issue while processing the authentication.</response>
     [AllowAnonymous]
     [HttpPost("login")]
     public async Task<IActionResult> Login(AuthenticateRequest model)
